bd7ea569cc8e370296c82e58de579c9997342756
Makoto Tokuyama <marugoshi@me.com>
2011-10-22 16:44:14 +0900

    EDIT: to use multiple layouts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+# -*- coding: utf-8 -*-
 module ApplicationHelper
+  # usage: <%= parent_layout "application" %>
+  def parent_layout(layout)
+    @_content_for[:layout] = self.output_buffer
+    self.output_buffer = render(:file => "layouts/#{layout}")
+  end
 end
