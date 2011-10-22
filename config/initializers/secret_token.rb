70b56d517944a2510ec966c727a83e5504d409de
Makoto Tokuyama <marugoshi@me.com>
2011-10-22 15:40:40 +0900

    ADD: after executing `bundle exec rails new .`.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reaper::Application.config.secret_token = '48096a4bb9592f24ad2e3569013b2969a3090e65c483d96b9d250d030913a719bb807ef05ff3b0ca7a2606c326b7ff21f068c1679771dad716eda6f068de388f'
