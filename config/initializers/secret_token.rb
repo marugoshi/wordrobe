c09b7159186ff5a4fcef802216ce3ea501bf9869
Makoto Tokuyama <marugoshi@me.com>
2011-10-22 16:02:43 +0900

    ADD: after executing `bundle exec rails new . --skip-gemfile --skip-bundle --database=postgresql --skip-test-unit --old-style-hash`.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reaper::Application.config.secret_token = '24ac62523d9339a50807e3f725ea6fa61cbf1446ae3390c99e39d5ce6a9f446b7745b8e5c55b9a967ca6c1c5dd4452ea7581453a1cc4acee051e1e3ee112d9d3'
