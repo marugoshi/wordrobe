c527e67bb468becc91d2bdaa5caaf6363ddff5c5
Makoto Tokuyama <marugoshi@me.com>
2011-10-23 03:14:29 +0900

    ADD: to insert word master data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Emanuel', :city => cities.first)
+file = Rails.root.join("db/masters/words.txt")
+File.open(file) {|f|
+  f.each { |word|
+    ActiveRecord::Base.transaction do
+      Word.create!(:word => word.strip)
+    end
+  }
+}
