fdd7ae5e769193c6e11e877b4831f1f83ad10552
Makoto Tokuyama <marugoshi@me.com>
2011-10-23 02:23:23 +0900

    ADD: after executing `bundle rails g model` then edit schema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111022100135) do
+ActiveRecord::Schema.define(:version => 20111022152919) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "uid",        :null => false
@@ -26,5 +26,13 @@ ActiveRecord::Schema.define(:version => 20111022100135) do
   add_index "accounts", ["nickname"], :name => "index_accounts_on_nickname"
   add_index "accounts", ["uid"], :name => "index_accounts_on_uid", :unique => true
   add_index "accounts", ["updated_at"], :name => "index_accounts_on_updated_at"
+
+  create_table "words", :force => true do |t|
+    t.string   "word",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "words", ["word"], :name => "index_words_on_word", :unique => true
 
 end
