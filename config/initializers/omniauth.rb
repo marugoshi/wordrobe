1388a8fb41ff526610b62a9f351b20437320714e
Makoto Tokuyama <marugoshi@me.com>
2011-10-23 14:46:52 +0900

    EDIT: to enable only facebook to login.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   # TODO : change callback url
-  provider :twitter, "PuSw5zuU8y1C9ksr1P4Gw", "wz01NRqZlwSDNtqAjW8JbDbh8GsD0QXjuXC1sasZvbI"
   provider :facebook, "156782621083661", "b3fbca92e9950d732c6e1220d62c8700"
 end  
