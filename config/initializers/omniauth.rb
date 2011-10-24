7a07570f3eaf04c1710ec9c02b3b50b87b700d02
Makoto Tokuyama <marugoshi@me.com>
2011-10-24 21:16:42 +0900

    EDIT: to use application offline.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,15 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # TODO : change callback url
   provider :facebook, "156782621083661", "b3fbca92e9950d732c6e1220d62c8700"
 end  
+
+if Rails.env == "development"
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:facebook] = {
+    "provider" => "facebook",
+    "uid" => 123456,
+    "user_info" => {
+      "name" => "Makoto Tokuyama",
+      "nickname" => "marugoshi",
+    },
+  }
+end
