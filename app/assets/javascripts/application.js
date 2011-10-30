6dc26c586a0a87a35479596cecf37ca6c0544da5
Makoto Tokuyama <marugoshi@me.com>
2011-10-31 00:02:14 +0900

    EDIT: move javascript to application.js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,11 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require autocomplete-rails
+
+// update wordrobe
+$(function() {
+  $(".update_wordrobe")
+    .live("ajax:success", function(event, data, xhr) {
+      $("#wordrobe").html(data);
+    });
+});
