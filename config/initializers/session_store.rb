f9f92b2f795a19471df401a74c93b721bd3a74b4
Makoto Tokuyama <marugoshi@me.com>
2011-11-12 20:43:26 +0900

    EDIT: add session expire(cookie base).

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Reaper::Application.config.session_store :cookie_store, :key => '_reaper_session'
+Reaper::Application.config.session_store :cookie_store, :key => '_reaper_session', :expire_after => 4.weeks
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
