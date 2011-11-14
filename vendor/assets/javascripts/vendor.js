f02580089ea0d713d92ca0f8def4781cca842334
Makoto Tokuyama <marugoshi@me.com>
2011-11-14 18:09:31 +0900

    ADD: jwerty.

--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -1,4 +1,5 @@
-//= require_tree ./star-rating
 //= require ./twitter-bootstrap/bootstrap-twipsy
 //= require ./twitter-bootstrap/bootstrap-popover
 //= require ./twitter-bootstrap/bootstrap-modal
+//= require_tree ./star-rating
+//= require_tree ./jwerty
