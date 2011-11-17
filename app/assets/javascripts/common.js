5d83d01a56b7a370736e8e291e82725e24f457a2
Makoto Tokuyama <marugoshi@me.com>
2011-11-18 01:53:46 +0900

    EDIT: add fade in/out effects.

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -5,6 +5,7 @@ $(function() {
     $(document).bind("keydown.right", "right", function() { next_page(); });
     $(document).bind("keydown.left", "left", function() { prev_page(); });
     $(document).bind("keydown.r", "r", function() { toggle_memorize(); });
+    $(document).bind("keydown.t", "t", function() { toggle_translate(); });
     // toggle_memorize();
   }
 
@@ -16,14 +17,33 @@ $(function() {
     $("div#add").modal({ show: true, keyboard: true, backdrop: true });
   }
 
+  var fade_out_heart = function() {
+    $("div#heart").fadeOut("slow");
+  }
+
+  var switch_wordrobe = function(data) {
+    $("div#wordrobe").html(data);
+  }
+
+  var fade_in_heart = function() {
+    $("div#heart").hide().fadeIn("slow");
+  }
+
   var next_page = function() {
     if ($("input#last_page").val() == "false") {
       var page = parseInt($("input#page").val());
+      
       $.ajax({
         type: "GET",
         url: "/accounts/wordrobes/wordrobes?page=" + (page + 1),
+        beforeSend: function() {
+          fade_out_heart();
+        },
         success: function(data) {
-          $("div#wordrobe").html(data);
+          switch_wordrobe(data);
+        },
+        complete: function() {
+          fade_in_heart();
         }
       });
     }
@@ -35,8 +55,14 @@ $(function() {
       $.ajax({
         type: "GET",
         url: "/accounts/wordrobes/wordrobes?page=" + (page - 1),
+        beforeSend: function() {
+          fade_out_heart();
+        },
         success: function(data) {
-          $("div#wordrobe").html(data);
+          switch_wordrobe(data);
+        },
+        complete: function() {
+          fade_in_heart();
         }
       });
     }
@@ -49,10 +75,14 @@ $(function() {
         type: "PUT",
         url: "/accounts/wordrobes/" + $("input#wordrobe_id").val() + "/toggle_memorize?page=" + page,
         success: function(data) {
-          $("div#wordrobe").html(data);
+          switch_wordrobe(data);
         }
       });
     }
+  }
+
+  var toggle_translate = function() {
+    
   }
 
   // bind event to help modal
